7b5503f696fe0eb8702cb0735a24e943075410fc
DaiDV <dangvandai1992@gmail.com>
2022-09-30 10:27:15 +0700

    Navigation Drawer:   widget.DrawerLayout + widget.NavigationView

--- app/src/main/java/net/xuanthulab/sqlitetutorial/ActionBar.java
+++ app/src/main/java/net/xuanthulab/sqlitetutorial/ActionBar.java
@@ -1,16 +1,47 @@
 package net.xuanthulab.sqlitetutorial;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class ActionBar extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_bar);
+
+        drawerLayout = findViewById(R.id.activity_main_drawer);
+        // làm nhiệm vụ điều khiển việc đóng mở DrawerLayout
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        // set icon hamburger xuất hiện trên ActionBar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    // Một số cấu hình của thiết bị có thể thay đổi khi ứng dụng đang chạy (ví dụ: hướng màn hình).
+    // Khi có sự thay đổi này, Android sẽ khởi động lại Activity (onDestroy() sẽ được gọi,
+    // sau đó là onCreate()) để điều chỉnh phù hợp với cấu hình mới.
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 
     // nạp main_actions vào ActionBar của Activity
@@ -24,6 +55,10 @@ public class ActionBar extends AppCompatActivity {
     // bắt event khi bấm vào các menu
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         switch (item.getItemId()) {
             case R.id.search:
                 Toast.makeText(this, "Search button selected", Toast.LENGTH_SHORT).show();
